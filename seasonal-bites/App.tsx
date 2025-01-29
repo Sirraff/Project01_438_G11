ad3d70377e40d2cb38e86c02dddab395bfc1b19d
OzzieMunoz <osvaldomunoz1205@gmail.com>
2025-01-29 09:25:46 -0800

    Add navigation for Search screen in App.tsx

--- seasonal-bites/App.tsx
+++ seasonal-bites/App.tsx
@@ -5,15 +5,17 @@ import { StyleSheet, Text, View } from 'react-native';
 import Login from './app/screens/Login';
 import CreateAccount from './app/screens/CreateAccount';
 import Menu from './app/screens/Menu';
+import TileSelector from "./app/screens/Search";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
+      <Stack.Navigator initialRouteName='Menu'>
         <Stack.Screen name='Login' component={Login} options={{headerShown: false}}/>
         <Stack.Screen name='CreateAccount' component={CreateAccount} options={{ headerShown: false }} />
         <Stack.Screen name='Menu' component={Menu} options={{ headerShown: false }} />
+        <Stack.Screen name='TileSelector' component={TileSelector} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
