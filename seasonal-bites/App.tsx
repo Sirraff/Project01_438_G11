c584653847fa08d333b62f56e17f41317867b0f3
OzzieMunoz <osvaldomunoz1205@gmail.com>
2025-02-01 22:39:27 -0800

    Implement Create Account functionality with Firebase Auth
    OA# Please enter the commit message for your changes. Lines starting

--- seasonal-bites/App.tsx
+++ seasonal-bites/App.tsx
@@ -11,7 +11,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Menu'>
+      <Stack.Navigator initialRouteName='Login'>
         <Stack.Screen name='Login' component={Login} options={{headerShown: false}}/>
         <Stack.Screen name='CreateAccount' component={CreateAccount} options={{ headerShown: false }} />
         <Stack.Screen name='Menu' component={Menu} options={{ headerShown: false }} />
