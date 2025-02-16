f6bb329d7852027c6f3fb590eaab9b454e1a638e
OzzieMunoz <osvaldomunoz1205@gmail.com>
2025-02-15 18:31:22 -0800

    Implement dark mode, Create light/dark themes

--- seasonal-bites/app/screens/Settings.tsx
+++ seasonal-bites/app/screens/Settings.tsx
@@ -1,53 +1,36 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, Switch } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../utils/navigation";
+import { getThemeStyles, useThemeToggle } from "../utils/Themes";
 
 type SettingsScreenNavigationProp = StackNavigationProp<RootStackParamList, "Settings">;
 
 const Settings: React.FC = () => {
     const navigation = useNavigation<SettingsScreenNavigationProp>();
+    const { isDarkMode, toggleTheme } = useThemeToggle();
+    const styles = getThemeStyles(isDarkMode);
 
     return (
         <View style={styles.container}>
+            <Text style={styles.title}>Settings</Text>
+
             <TouchableOpacity
                 style={styles.button}
                 onPress={() => navigation.navigate("LocationSettings")}
             >
                 <Text style={styles.buttonText}>Set Location</Text>
             </TouchableOpacity>
+
+            <View
+                style={{ flexDirection: "row", alignItems: "center", marginBottom: 20 }}
+            >
+                <Text style={styles.text}>Dark Mode</Text>
+                <Switch value={isDarkMode} onValueChange={toggleTheme} />
+            </View>
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 20,
-        backgroundColor: "#f5f5f5",
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: "bold",
-        marginBottom: 20,
-    },
-    button: {
-        backgroundColor: "#2d936c",
-        borderRadius: 8,
-        paddingVertical: 12,
-        paddingHorizontal: 40,
-        marginVertical: 5,
-        width: "80%",
-        alignItems: "center",
-    },
-    buttonText: {
-        color: "white",
-        fontSize: 16,
-        fontWeight: "600",
-    },
-});
 
 export default Settings;
